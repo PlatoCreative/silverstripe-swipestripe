46a5e24cf218db66c9fb5d17c1c39082ff9de513
Will <will@platocreative.co.nz>
2015-07-28 14:28:55 +1200

    updating email function

--- code/order/Order.php
+++ code/order/Order.php
@@ -887,27 +887,30 @@ class Order_Update extends DataObject {
 		$shopConfig = ShopConfig::current_shop_config();
 
 		// Update the Order, setting the same status
-		if ($this->Status) {
+		if($this->Status){
 			$order = $this->Order();
-			if ($order->exists()) {
+			if($order->exists()){
 				if($order->Status != $this->Status){
+					// Email the customer about update
+					if($this->Visible){
+						$to = $order->Member()->Email;
+						$from = $shopConfig->NotificationTo;
+						$subject = $siteconfig->Title . ' - Order #' . $order->ID . ' Update';
+
+						$body = "<p>Hi " . $order->Member()->FirstName . ",</p>";
+						$body .= "<p>There has been the following update to your order #" . $order->ID . ".</p>";
+						$body .= "<p><strong>Order Status:</strong> " . $this->Status . "</p>";
+						$body .= $this->Note ? "<p><strong>Note:</strong> " . $this->Note . "</p>" : "";
+						$body .= "<p>You can view this order by visiting the following URL<br />";
+						$body .= "<a href='" . Director::absoluteBaseURL() . "account/order/" . $order->ID . "' target='_blank'>" . Director::absoluteBaseURL() . "account/order/" . $order->ID . "</a></p>";
+
+						$email = new Email($from, $to, $subject, $body);
+						$email->send();
+					}
+
+					// Update the order status
 					$order->Status = $this->Status;
 					$order->write();
-
-					// Email the customer about update
-					$to = $order->Member()->Email;
-					$from = $shopConfig->NotificationTo;
-					$subject = $siteconfig->Title . ' - Order #' . $order->ID . ' Update';
-
-					$body = "<p>Hi " . $order->Member()->FirstName . ",</p>";
-					$body .= "<p>There has been the following update to your order #" . $order->ID . ".</p>";
-					$body .= "<p><strong>Order Status:</strong> " . $this->Status;
-					$body .= $this->Note ? "<br />" . $this->Note . "<br />" : "";
-					$body .= "</p>";
-					$body .= "<p>You can view this order by visiting the following URL<br />";
-					$body .= "<a href='" . Director::absoluteBaseURL() . "account/order/" . $order->ID . "' target='_blank'>" . Director::absoluteBaseURL() . "account/order/" . $order->ID . "</a></p>";
-
-					$email = new Email($from, $to, $subject, $body);
 				}
 			}
 		}
